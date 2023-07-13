0893cd3c580b9b4343482f6dabe29e51935ea8f4
Devin <devinhill2911@gmail.com>
2023-07-13 12:13:11 -0400

    setting variables

---
+++ Assets/js/script.js
@@ -0,0 +1,8 @@
+var startButton = document.getElementById('start Button'); startButton.onclick = startQuiz;
+var timerLabel = document.getElementById('timer');
+var questionSection = document.getElementById('question-section');
+var scoreLabel = document.getElementById('score');
+var initialInput = document.getElementById('initial-input');
+var saveButton = document.getElementById('save-btn');
+var highscoreSection = document.getElementById('highscore-section');
+
